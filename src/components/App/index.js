ecf873d999b44c07a7ca3d7d97790e7e457af392
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2018-12-02 19:06:48 +0900

    Setup react

---
+++ src/components/App/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './index.css';
+
+const App = () => <div><p className='app-label'>Welcome React</p></div>;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
