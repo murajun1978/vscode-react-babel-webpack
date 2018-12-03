df49a757fa85ddc67b423625ce82277fa3b4bc6c
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2018-12-03 21:53:16 +0900

    Add counter to App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import './index.css';
 
-const App = () => <div><p className='app-label'>Welcome React!1</p></div>;
+const increment = (state, props) => ({ counter: state.counter + 1 });
+const decrement = (state, props) => ({ counter: state.counter - 1 });
+
+class App extends React.Component {
+  state = { counter: 0 };
+
+  onIncrement = () => this.setState(increment);
+  onDecrement = () => this.setState(decrement);
+
+  render() {
+    return (
+      <div>
+        <div>
+          <label>{this.state.counter}</label>
+        </div>
+        <button onClick={this.onIncrement}>+</button>
+        <button onClick={this.onDecrement}>-</button>
+      </div>
+    );
+  }
+};
 
 export default App;
