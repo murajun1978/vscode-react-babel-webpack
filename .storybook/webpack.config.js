6077f8f05793b788b63531a099535b91c650afc2
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2018-12-06 00:59:05 +0900

    Setup Storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,18 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
+};
